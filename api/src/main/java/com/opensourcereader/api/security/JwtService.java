35462b62d16f2dd1ef481e9da0f5ea0243298826
JongwonLee <gudtksfl@gmail.com>
2025-11-30 20:41:43 +0900

    feat: 보안을 위한 SecurityConfig 및 JwtService 생성

---
+++ api/src/main/java/com/opensourcereader/api/security/JwtService.java
@@ -0,0 +1,15 @@
+package com.opensourcereader.api.security;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+
+@Service
+public class JwtService {
+
+  private String secretKey;
+
+  private Algorithm algorithm;
+  private JWTVerifier jwtVerifier;
+}
