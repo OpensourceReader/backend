419d18d9008001049c62e1638b53593eb8ec9e3e
JongwonLee <gudtksfl@gmail.com>
2025-11-30 19:46:22 +0900

    feat: User 도메인 초기 구조 및 스켈레톤 코드 생성
    [구조 설정]
    - API 모듈: UserControler, UserFacadeService
    - Core 모듈: User(Entity), UserRepository, UserService
    - 각 계층별 빈 클래스(Skeleton) 생성 및 패키지 구조 확보

---
+++ api/src/main/java/com/opensourcereader/api/controller/user/UserController.java
@@ -0,0 +1,15 @@
+package com.opensourcereader.api.controller.user;
+
+import com.opensourcereader.api.facade.user.UserFacadeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+  private final UserFacadeService userFacadeService;
+
+}
