b59d45d1ba5afaccabc651a8cdecea66bf7b6c4c
JongwonLee <gudtksfl@gmail.com>
2025-12-03 16:14:13 +0900

    feat: CORS Configurer 설정
    - 배포 전 원활한 개발을 위한 초기 셋팅

---
+++ api/src/main/java/com/opensourcereader/api/security/CorsConfigurer.java
@@ -0,0 +1,50 @@
+package com.opensourcereader.api.security;
+
+import java.util.List;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/** CORS(Cross-Origin Resource Sharing, 교차 출처 리소스 공유)에 대한 설정 */
+@Component
+@Order(0)
+public class CorsConfigurer implements SecurityConfigurer {
+
+  @Override
+  public void configure(HttpSecurity http) {
+    try {
+      http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+    } catch (Exception e) {
+      throw new RuntimeException("CORS Error", e);
+    }
+  }
+
+  protected CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    // 모든 도메인 요청에 대해 허용
+    configuration.setAllowedOriginPatterns(List.of("*"));
+    // HTTP 헤더 허용값
+    configuration.setAllowedHeaders(
+        List.of(
+            HttpHeaders.AUTHORIZATION,
+            HttpHeaders.CONTENT_TYPE,
+            HttpHeaders.ACCEPT,
+            HttpHeaders.COOKIE));
+
+    // 자격 증명 허용 설정
+    configuration.setAllowCredentials(true);
+
+    // HTTP 메서드 허용 설정
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    // "/api/**" 패턴으로 들어오는 모든 요청에 위 CORS 환경 설정 적용
+    source.registerCorsConfiguration("/api/**", configuration);
+    return source;
+  }
+}
