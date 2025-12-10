3323cd2057199af77e3cb92dd8cf8a83d9ade6a6
JongwonLee <gudtksfl@gmail.com>
2025-12-10 18:25:32 +0900

    chore: 유저 엔티티 내 Column 추가

--- core/src/main/java/com/opensourcereader/core/user/entity/User.java
+++ core/src/main/java/com/opensourcereader/core/user/entity/User.java
@@ -1,6 +1,7 @@
 package com.opensourcereader.core.user.entity;
 
 import com.opensourcereader.core.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -20,26 +21,35 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
-  private String providerId;
+  @Column private String providerId;
 
   @Enumerated(EnumType.STRING)
   private Role role;
 
-  private String username;
+  @Column private String username;
 
+  @Column(nullable = false)
   private String nickname;
 
+  @Column(nullable = false)
   private String email;
 
+  @Column(nullable = false)
   private String password;
 
-  private String avatarUrl;
+  @Column private String avatarUrl;
 
+  @Column(nullable = false)
   private Boolean disabled;
 
   public static UserBuilder of(String nickname, String email, String password) {
 
-    return User.builder().nickname(nickname).email(email).password(password).disabled(false);
+    return User.builder()
+        .role(Role.USER)
+        .nickname(nickname)
+        .email(email)
+        .password(password)
+        .disabled(false);
   }
 
   public void updateAvatar(String newAvatarUrl) {
