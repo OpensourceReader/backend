f144c1d00c45e701acc917cf1b7cda7d26d3c21d
JongwonLee <gudtksfl@gmail.com>
2025-12-22 14:10:55 +0900

    refactor: Json 설정 파일 수정
    - 모르는 필드가 오더라도 오류 안 던지게 만듬
    - 타임 모듈 등록

--- api/src/main/java/com/opensourcereader/api/config/JsonConfig.java
+++ api/src/main/java/com/opensourcereader/api/config/JsonConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class JsonConfig {
 
   @Bean
   public ObjectMapper objectMapper() {
-    return new ObjectMapper().enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    return new ObjectMapper()
+        .registerModule(new JavaTimeModule())
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 }
