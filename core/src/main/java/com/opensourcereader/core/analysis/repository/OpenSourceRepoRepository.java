99c7e110d7c57c3edbabfb481835b13e7a17491e
GusanHaribo <hth99131@gmail.com>
2025-12-23 14:16:52 +0900

    refactor: 사용하지 않는 레포지토리 클래스 삭제 및 수정

--- core/src/main/java/com/opensourcereader/core/analysis/repository/OpenSourceRepoRepository.java
+++ core/src/main/java/com/opensourcereader/core/analysis/repository/OpenSourceRepoRepository.java
@@ -1,7 +1,5 @@
 package com.opensourcereader.core.analysis.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.opensourcereader.core.analysis.entity.OpenSourceRepo;
@@ -9,6 +7,4 @@ import com.opensourcereader.core.analysis.entity.OpenSourceRepo;
 public interface OpenSourceRepoRepository extends JpaRepository<OpenSourceRepo, Long> {
 
   boolean existsByCloneUrl(String cloneUrl);
-
-  Optional<OpenSourceRepo> findByCloneUrl(String cloneUrl);
 }
