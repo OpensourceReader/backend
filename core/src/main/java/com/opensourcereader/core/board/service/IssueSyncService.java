aaf95ae3e05ba9c81c32876e93579c91b5fb1cb4
JongwonLee <gudtksfl@gmail.com>
2025-12-30 19:00:54 +0900

    feat: Issue Sync 로직 추가
    - 리스트에 담아서 한 번에 저장하거나 업데이트

--- core/src/main/java/com/opensourcereader/core/board/service/IssueSyncService.java
+++ core/src/main/java/com/opensourcereader/core/board/service/IssueSyncService.java
@@ -1,5 +1,8 @@
 package com.opensourcereader.core.board.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +15,7 @@ import com.opensourcereader.core.board.exception.IssueNotFoundException;
 import com.opensourcereader.core.board.repository.IssueRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.yaml.snakeyaml.util.Tuple;
 
 @Service
 @RequiredArgsConstructor
@@ -37,6 +41,32 @@ public class IssueSyncService {
     }
   }
 
+  @Transactional
+  public void syncIssues(Queue<BoardBaseCommand> commands) {
+    List<BoardBaseCommand> saveCommands = new ArrayList<>();
+    List<PairIssue> updateCommands = new ArrayList<>();
+
+    while (!commands.isEmpty()) {
+      BoardBaseCommand command = commands.poll();
+      try {
+        Issue existingIssue =
+            issueRepository
+                .findByRepositoryIdAndTagId(command.getRepo().getId(), command.getTagId())
+                .orElse(null);
+        if (existingIssue == null) {
+          saveCommands.add(command);
+        } else {
+          updateCommands.add(new PairIssue(existingIssue, command));
+        }
+      } catch (Exception e) {
+        commands.add(command);
+      }
+    }
+
+    saveNesIssues(saveCommands);
+    updateIssues(updateCommands);
+  }
+
   private void saveNewIssue(BoardBaseCommand command) {
     Issue entity;
     if (command instanceof PullCommand pullCommand) {
@@ -45,6 +75,20 @@ public class IssueSyncService {
       entity = Issue.from(command);
     }
     issueRepository.save(entity);
+  }
+
+  private void saveNesIssues(List<BoardBaseCommand> commands) {
+    List<Issue> entityList = new ArrayList<>();
+    for (BoardBaseCommand command : commands) {
+      Issue entity;
+      if (command instanceof PullCommand pullCommand) {
+        entity = Pull.from(pullCommand);
+      } else {
+        entity = Issue.from(command);
+      }
+      entityList.add(entity);
+    }
+    issueRepository.saveAll(entityList);
   }
 
   private void retryUpdate(BoardBaseCommand command) {
@@ -63,4 +107,13 @@ public class IssueSyncService {
       pull.updateReviewCount(pullCommand.getReviewCount());
     }
   }
+
+  private void updateIssues(List<PairIssue> updatePair) {
+    for (PairIssue pair : updatePair) {
+      updateIssue(pair.issue(), pair.command());
+    }
+  }
+}
+
+record PairIssue(Issue issue, BoardBaseCommand command) {
 }
