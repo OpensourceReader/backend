d9dcda5452929b3fbfd0ab54fd4dfceeadd08d4f
GusanHaribo <hth99131@gmail.com>
2026-01-08 12:54:03 +0900

    feat: MethodPolymorphicDispatchService 다형성 상속 연결기능 추가

--- core/src/main/java/com/opensourcereader/core/analysis/service/impl/callgraph/MethodPolymorphicDispatchService.java
+++ core/src/main/java/com/opensourcereader/core/analysis/service/impl/callgraph/MethodPolymorphicDispatchService.java
@@ -27,40 +27,49 @@ public class MethodPolymorphicDispatchService {
   private final DeclaredTypeRepository declaredTypeRepository;
 
   public List<CodeMethodCallEdge> dispatch(Long repoId) {
+    List<CodeMethodCallEdge> codeMethodCallEdges = new ArrayList<>();
     List<DeclaredType> interfaceTypes =
         declaredTypeRepository.findByRepoAndTypesByKind(repoId, TypeKind.INTERFACE);
-    List<CodeMethodCallEdge> codeMethodCallEdges = new LinkedList<>();
     for (DeclaredType interfaceType : interfaceTypes) {
-      codeMethodCallEdges.addAll(interfaceDispatch(interfaceType));
+      codeMethodCallEdges.addAll(dispatchByBfs(interfaceType, true));
     }
+    List<DeclaredType> classTypes =
+        declaredTypeRepository.findByRepoAndTypesByKind(repoId, TypeKind.CLASS);
+    for (DeclaredType classType : classTypes) {
+      codeMethodCallEdges.addAll(dispatchByBfs(classType, false));
+    }
+
     return codeMethodCallEdgeRepository.saveAll(codeMethodCallEdges);
   }
 
-  public List<CodeMethodCallEdge> interfaceDispatch(DeclaredType interfaceType) {
+  private List<CodeMethodCallEdge> dispatchByBfs(DeclaredType type, boolean isInterface) {
     List<CodeMethodCallEdge> result = new ArrayList<>();
     Queue<DeclaredType> queue = new LinkedList<>();
-    queue.add(interfaceType);
+    queue.add(type);
 
     while (!queue.isEmpty()) {
       int n = queue.size();
       for (int i = 0; i < n; i++) {
         DeclaredType polledType = queue.poll();
         for (DeclaredTypeImplementEdge implementEdge : polledType.getImplementations()) {
-          DeclaredType nextInterfaceType = implementEdge.getInterfaceType();
+          DeclaredType declaredType = implementEdge.getInterfaceType();
           Map<DeclaredMethod, DeclaredMethod> collect =
-              nextInterfaceType.getDeclaredMethods().stream()
+              declaredType.getDeclaredMethods().stream()
                   .collect(Collectors.toMap(it -> it, it -> it));
           for (DeclaredMethod declaredMethod : polledType.getDeclaredMethods()) {
             DeclaredMethod internalInheritance =
-                DeclaredMethod.internalInheritance(declaredMethod, nextInterfaceType);
+                DeclaredMethod.internalInheritance(declaredMethod, declaredType);
             DeclaredMethod nextDeclaredMethod =
                 collect.getOrDefault(declaredMethod, internalInheritance);
             result.add(CodeMethodCallEdge.of(declaredMethod, nextDeclaredMethod));
           }
 
           //          visited 없어서 중복 방문/무한 루프 위험, 인터페이스는 보통은 dag
-          if (nextInterfaceType.getTypeKind().equals(TypeKind.INTERFACE)) {
-            queue.add(nextInterfaceType);
+          if (isInterface && declaredType.getTypeKind().equals(TypeKind.INTERFACE)) {
+            queue.add(declaredType);
+          }
+          if (!isInterface && declaredType.getTypeKind().equals(TypeKind.CLASS)) {
+            queue.add(declaredType);
           }
         }
       }
@@ -68,6 +77,4 @@ public class MethodPolymorphicDispatchService {
 
     return result;
   }
-
-  // 다음은 상속, 상속할떄는 클래스만따로 진행
 }
